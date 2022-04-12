93719fb33cc7af9462423e08e4ec66b6541ff522
marcelojunior1 <marcelojunior@usp.br>
2022-04-12 08:36:22 -0300

    Adicionado projeto ao front end

--- geocomp/lineintersections/__init__.py
+++ geocomp/lineintersections/__init__.py
@@ -1,5 +1,8 @@
 from . import brute_force
+from . import projeto2
 
-children = [('brute_force', 'Brute_force', 'Forca\nBruta')]
+
+children = [('brute_force', 'Brute_force', 'Forca\nBruta'), 
+			('projeto2', 'Projeto2', 'Interseção-SH-P2')]
 
 __all__ = [a[0] for a in children]
---
+++ geocomp/lineintersections/projeto2.py
@@ -0,0 +1,10 @@
+from geocomp.common import prim
+from geocomp.common import segment
+from geocomp.common import control
+from geocomp import config
+
+
+
+def Projeto2(l):
+    print("OK")
+
